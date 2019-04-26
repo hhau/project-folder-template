fc98da9ac8dabfda59b4fa7bd279baf9380b40b7
Andrew Manderson <a.manderson@live.co.uk>
2019-04-26 12:03:26 +0100

    missed a .gitignore

--- run-me-after-copy.sh
+++ run-me-after-copy.sh
@@ -3,6 +3,9 @@
 # delete all the .gitignore files not in the top level directory
 trash ./*/.gitignore
 
+# the above misses the one in scripts/common
+trash ./scripts/common/.gitignore
+
 # delete the current git setup and initialise a new one
 trash ./.git
 git init
