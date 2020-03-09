5346bf961b6eedd01b36f66632d2244579a89bce
Andrew Manderson <a.manderson@live.co.uk>
2020-03-09 09:57:49 +0000

    add that util function I always seem to need, change the csl file + bib structure

---
+++ scripts/common/mcmc-util.R
@@ -0,0 +1,25 @@
+mcmc_list_to_array <- function(mcmc_list) {
+  stopifnot(class(mcmc_list) == "mcmc.list")
+  
+  n_iter <- nrow(mcmc_list[[1]])
+  n_par <- ncol(mcmc_list[[1]])
+  n_chain <- length(mcmc_list)
+  par_names <- dimnames(mcmc_list[[1]])[[2]]
+  
+  results_array <- array(
+    data = NA,
+    dim = c(n_iter, n_chain, n_par),
+    dimnames = list(
+      NULL,
+      sprintf("chain_%d", 1:n_chain),
+      par_names
+    )
+  )
+  
+  for (chain_index in 1:n_chain) {
+    results_array[, chain_index,] <- results[[chain_index]]
+  }
+  
+  return(results_array)
+  
+}
