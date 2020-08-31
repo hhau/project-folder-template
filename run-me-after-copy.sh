55b1c150eec4516400e248ee2b4871645ca137a0
Andrew Manderson <a.manderson@live.co.uk>
2020-08-31 10:17:55 +0100

    adjust initialisation script

--- run-me-after-copy.sh
+++ run-me-after-copy.sh
@@ -14,16 +14,18 @@ git init
 # remove the REAMDE that is just for github
 trash README.md
 
+# do this before echoing out the 
+Rscript -e "renv::init()"
+
 echo "
   Change the following things: 
     1: The name of the .rmd file
     2: The name of the corresponding .tex file in the .gitignore 
     3: The entry in the makefile called BASENAME
     4: The name of the .bib file in bibliography/
+      4.1: The path to the renamed bibliography in the .rmd file
   These should all match. 
-  "
-
-Rscript -e "renv::init()"
+  "a
 
 # delete itself
 trash $0
