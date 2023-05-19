a0049acf66f36870b9d2c10fe667f80ef176eb7a
Andrew Manderson <a.manderson@live.co.uk>
2023-05-19 16:45:28 +0100

    switch to logger

--- scripts/common/setup-logger.R
+++ scripts/common/setup-logger.R
@@ -1,26 +1,26 @@
-suppressPackageStartupMessages(library(futile.logger))
+suppressPackageStartupMessages(library(logger))
 suppressPackageStartupMessages(library(crayon))
 
 base_filename <- unlist(strsplit(x = basename(Sys.glob("./*.rmd")), ".rmd"))
 log_filename <- paste0("logs/" , Sys.Date(), "_", base_filename, ".log")
-flog.appender(appender.file(log_filename), name = base_filename)
+log_appender(appender_file(log_filename), namespace = base_filename)
 
 cat(
-  blue(sprintf('futile.logger info')),
+  blue(sprintf('logger info')),
   "\n",
   "  ",
   blue$underline("log_filename:"),
   sprintf("%s", log_filename),
   "\n",
   "  ",
-  blue$underline("base_filename:"),
+  blue$underline("base_filename / namespace :"),
   sprintf("%s", base_filename),
   "\n"
 )
 
 logfile_line_delineator <- function() {
-  flog.info(
+  log_info(
     paste0(paste0(rep("=", times = 40), collapse = ""), "\n"),
-    name = base_filename
+    namespace = base_filename
   )
 }
